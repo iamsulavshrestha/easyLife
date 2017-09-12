51ae9af9c84df268ccb64b85c7285d13c21ae410
Sulav Shrestha <iamsulavshrestha@gmail.com>
2017-09-12 14:39:07 +1200

    added some images that i need mostly

--- dockerImages.sh
+++ dockerImages.sh
@@ -1,5 +1,5 @@
 ## now loop through the above array
-for i in "mariadb" "node"
+for i in "mariadb" "node" "httpd:alpine" "alpine" "docker" "swarm" "nginx" "redis" "ubuntu" "mysql" "mongo" "memcached" "wordpress" "ruby" "python" "php" "jenkins" "haproxy" "drupal" "joomla" "mono" "gradle" "couchbase" "blackfire" "mssql-server-linux" "eclipse-che"
 do
        docker pull "$i"
           # or do whatever with individual element of the array
