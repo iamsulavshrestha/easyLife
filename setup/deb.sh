0fea6c9d78e39c64fda518f588fdc823383757c3
Sulav Shrestha <iamsulavshrestha@gmail.com>
2017-09-19 16:06:26 +1200

    refactored scripts

---
+++ setup/deb.sh
@@ -0,0 +1,4 @@
+. "$PWD/setup/deb/chrome.sh"
+. "$PWD/setup/deb/viber.sh"
+. "$PWD/setup/deb/smartgit.sh"
+
---
+++ setup/deb/viber.sh
@@ -0,0 +1,3 @@
+wget -P ~/Downloads/ http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+dpkg -i install ~/Downloads/viber.deb
+apt install -f
