51481676d1c4297ff7691fa121543093233c32ea
Sulav Shrestha <iamsulavshrestha@gmail.com>
2017-09-19 13:33:29 +1200

    added docker script + working on kvm and virtualbox scripts

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 apt update
 apt upgrade -y
+. setup/apt.sh
 # setting up vim
 echo "installing and setting up vim"
 . setup/vim.sh
 . setup/chrome.sh
-#. setup/docker.sh
 . setup/viber.sh
+. setup/docker.sh
--- tmp/aptPackages.sh
+++
@@ -1,5 +0,0 @@
-## now loop through the above array
-for i in "git" "vim"
-do
-       sudo apt install "$i"          
-done      
