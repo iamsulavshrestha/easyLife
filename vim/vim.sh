1ee5cdca8dbda118c7b2b777725490cab03248c0
Sulav Shrestha <iamsulavshrestha@gmail.com>
2019-03-31 01:04:08 +1300

    using vivid manager

--- vim/vim.sh
+++ vim/vim.sh
@@ -1,11 +1,10 @@
-if [ ! -e "$HOME/.vim/bundle/Vundle.vim" ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -e "$HOME/.vim/pack/vivid/opt/Vivid.vim" ]; then
+#    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/axvr/vivid.vim ~/.vim/pack/vivid/opt/Vivid.vim
 fi
 if [ -e "$HOME/.vimrc" ]; then
     rm "$HOME/.vimrc"
 fi
-if [ ! -h "$HOME/.vimrc" ]; then
-#    ln -s  ~/workspaces/bash/conf/vimrc ~/.vimrc
-    ln -s "$PWD/conf/vimrc" ~/.vimrc
+
+    cp "$PWD/vimrc" ~/.vimrc
     vim +PluginInstall +qall
-fi
