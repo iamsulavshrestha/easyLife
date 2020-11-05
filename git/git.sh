621f4ae850c17f7c314bc60bc3d1c46e7d1090ec
Sulav Shrestha <iamsulavshrestha@gmail.com>
2020-11-05 11:19:49 +0545

    added git configs

--- git/git.sh
+++ git/git.sh
@@ -1,2 +1,4 @@
 git config --global credential.helper 'cache --timeout=78600'
 git config --global help.autocorrect 1
+git config --global user.email "iamsulavshrestha@gmail.com"
+git config --global user.name "Sulav Shrestha"
