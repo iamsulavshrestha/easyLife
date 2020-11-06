a669b3a1422bb2b28468e7fee07a58c5f73c21a2
Sulav Shrestha <iamsulavshrestha@gmail.com>
2020-11-06 18:42:47 +0545

    fixed minor git ignore global

--- git/git.sh
+++ git/git.sh
@@ -2,5 +2,5 @@ git config --global credential.helper 'cache --timeout=78600'
 git config --global help.autocorrect 1
 git config --global user.email "iamsulavshrestha@gmail.com"
 git config --global user.name "Sulav Shrestha"
-cp $PWD/gitignore ~/.gitignore
-git config --global core.excludesFile '~/.gitignore'
+cp $PWD/gitignore ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
