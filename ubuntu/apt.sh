4d53e89bf1340882bf5594734f3ce02fac5b69bf
Sulav Shrestha <iamsulavshrestha@gmail.com>
2020-11-13 13:53:16 +0545

    minor fixes

--- ubuntu/apt.sh
+++ ubuntu/apt.sh
@@ -1,3 +1,3 @@
 apt update -y &&
 apt upgrade -y &&
-apt install -y curl git openssh nodejs php yarn apache2 php mariadb sqlite python vim
+apt install -y curl git openssh nodejs php yarn apache2 php mariadb sqlite python vim php-apache
