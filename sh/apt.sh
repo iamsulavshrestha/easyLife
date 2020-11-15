a30e3efeedbd904b935f2977647b2cc6dffb87f3
Sulav Shrestha <connect@sulav.name.np>
2020-11-15 21:20:47 +0545

    minor fixes

--- sh/apt.sh
+++ sh/apt.sh
@@ -1,5 +1,6 @@
 apt update -y &&
 apt upgrade -y &&
-apt install -y curl git openssh nodejs php yarn apache2 php mariadb sqlite python vim php-apache
+apt install -y curl git openssh nodejs php apache2 php mariadb sqlite python vim php-apache
 # ================= YARN PACKAGES =============
-yarn global add heroku commitizen
+npm install -g yarn
+yarn global add heroku commitizen firebase-tools create-react-app
