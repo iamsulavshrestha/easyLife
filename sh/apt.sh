310cd2efb026c15ec7ce3bab592f75173fac0dbc
Sulav Shrestha <connect@sulav.name.np>
2020-11-18 11:40:27 +0545

    added few npm projects

--- sh/apt.sh
+++ sh/apt.sh
@@ -3,4 +3,4 @@ apt upgrade -y &&
 apt install -y curl git openssh nodejs php apache2 php mariadb sqlite python vim php-apache
 # ================= YARN PACKAGES =============
 npm install -g yarn
-yarn global add heroku commitizen firebase-tools create-react-app
+yarn global add heroku commitizen firebase-tools create-react-app express express-generator
