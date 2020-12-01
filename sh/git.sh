e29cc59e8a750b655730278cc22de4adf6d8bce5
Sulav Shrestha <connect@sulav.name.np>
2020-12-01 14:57:26 +0545

    add git aliases

--- sh/git.sh
+++ sh/git.sh
@@ -3,4 +3,5 @@ git config --global help.autocorrect 1
 git config --global user.email "connect@sulav.name.np"
 git config --global user.name "Sulav Shrestha"
 cp $PWD/files/gitignore ~/.gitignore_global
+cp $PWD/files/gitconfig ~/.gitconfig
 git config --global core.excludesfile ~/.gitignore_global
