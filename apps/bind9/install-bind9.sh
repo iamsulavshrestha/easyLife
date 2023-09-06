311e1d6078ea3c155a0a3dc1956fa3ac67c0ae4d
Sulav Shrestha <iamsulavshrestha@gmail.com>
2023-09-06 18:36:25 +1200

    updates

--- apps/bind9/install-bind9.sh
+++ apps/bind9/install-bind9.sh
@@ -5,5 +5,5 @@ confpath=./conf
 
 ## Commands start
 apt install bind9
-cp /etc/bind9/named.conf.options /etc/bind9/named.conf.options.original
-cp "${confpath}/named.conf.options" /etc/bind9/named.conf.options
+cp /etc/bind/named.conf.options /etc/bind/named.conf.options.original
+cp "${confpath}/named.conf.options" /etc/bind/named.conf.options
