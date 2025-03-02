2326aa6d35903e12d1a7a544f4514444ed21fdac
Sulav Shrestha <iamsulavshrestha@gmail.com>
2025-03-03 10:43:24 +1300

    add backup script

---
+++ sh/backup-data.sh
@@ -0,0 +1,1 @@
+tar -zcvf /media/sulav/storage/Sulav/Backups/data/data_backup_`da    te +%Y-%m-%d`.tar.bz2 --listed-incremental=/media/sulav/storage/Sulav/Backup    s/data/backup_log.txt /home/pi/data
